5b6f78bce57967f9a5645fff6f05c8714aa6c725
GkirillS <kirill_gorbachyov050201@mail.ru>
2024-02-27 10:55:57 +0300

    for gh-pages

--- src/helpers/const.js
+++
@@ -1,5 +0,0 @@
-import md5 from "blueimp-md5"
-
-export const API = 'https://api.valantis.store:41000/'
-export const LIMIT = 50
-export const PASSWORD = md5(`Valantis_${new Date().toLocaleDateString().split('.').reverse().join('')}`)
